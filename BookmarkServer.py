0dedce07867fcae8002c126f74b90c0dec575d5d
Sandeep <sandeep@batch.ly>
2017-12-15 12:39:16 +0530

    Server modified to pick port from Heroku environment

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -43,6 +43,7 @@
 
 import http.server
 import requests
+import os
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -149,6 +150,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.wfile.write("Requested url doesn't exist".encode())
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
